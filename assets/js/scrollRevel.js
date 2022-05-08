73f565d7381f330c736c3d5ef7db6c5e28c665e7
y1r1tzy <yiritzygpm@gmail.com>
2022-05-08 16:05:43 -0500

    perrito

--- assets/js/scrollRevel.js
+++ assets/js/scrollRevel.js
@@ -1,3 +1,12 @@
 ScrollReveal().reveal('#imagenes',{delay: 500});
 ScrollReveal().reveal('#video',{delay: 500});
 ScrollReveal().reveal('#adoptar',{delay: 500});
+ScrollReveal().reveal('#formulario',{delay: 500});
+ScrollReveal().reveal('#tabla',{delay: 500});
+ScrollReveal().reveal('#perritoo',{delay: 500});
+
+
+
+
+
+
